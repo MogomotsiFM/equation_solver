9ccc99552497ba9dee192b438ffd4cd25fb8831b
Advice <amg.seiphemo@gmail.com>
2024-04-27 23:05:42 +0200

    Added setup.py to make the package installable.

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='EquationSolver',
+      version='0.0.1',
+      description='Solves linear polynomial problems of up to order 2.',
+      packages=['equation_solver'],
+     )
