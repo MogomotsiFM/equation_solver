a0aab0e705c4f1c0c8c84e02dab0a9f385728ae0
Advice <amg.seiphemo@gmail.com>
2024-07-23 22:16:34 +0200

    Modified one of the test because it fails when computing code coverage metrics.

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,7 @@ q = '( x - 1 )( 4x + 3 ) + 105x - 9 = - 12( - 12x + 10 ) + 12'
 # q = 'x^2 + 1'
 # q = 'x^3 - 1 = 0'
 
-if len(sys.argv) > 1:
+if __name__ == "__main__" and len(sys.argv) > 1:
     question = sys.argv[1:]
     
     q = " ".join(question)
